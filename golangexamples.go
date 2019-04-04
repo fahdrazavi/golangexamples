86c0e3153d396c48c5e4cf66e594a430fd950073
fahdrazavi <fahdrazavi@gmail.com>
2019-04-04 15:08:13 +0500

    It works. :)

---
+++ golangexamples.go
@@ -0,0 +1,24 @@
+package golangexamples
+
+import (
+	"github.com/ehteshamz/greetings"
+)
+
+func ConcatSlice(sliceToConcat []byte) string {
+	str := string(sliceToConcat[0])
+	for _, c := range sliceToConcat[1:] {
+		str += "-"
+		str += string(c)
+	}
+	return str
+}
+
+func Encrypt(sliceToEncrypt []byte, ceaserCount int) {
+	for i, c := range sliceToEncrypt {
+		sliceToEncrypt[i] = (((c + byte(ceaserCount)) - 97) % 26) + 97
+	}
+}
+
+func EZGreetings(name string) string {
+	return greetings.PrintGreetings(name)
+}
